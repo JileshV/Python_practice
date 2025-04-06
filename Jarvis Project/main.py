5994d4430459131c2371edd08d724f3c68013ffa
JileshV <jilesh.vaghela@gmail.com>
2025-04-06 15:43:05 +0530

    updates

--- Jarvis Project/main.py
+++ Jarvis Project/main.py
@@ -9,21 +9,38 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+def openCommand(c):
+    if "open youtube" in c.lower():
+        webbrowser.open("http://youtube.com")
+    if "open linkedin" in c.lower():
+        webbrowser.open("http://linkedin.com")
+    if "play rang" in c.lower():
+        webbrowser.open("https://www.youtube.com/watch?v=OdrOp7JwZiE&ab_channel=SaregamaMusic")
+
 if __name__ == "__main__":
     speak("Hello from Jarvis")
 
     while True:
         #Listening to auido
         r = sr.Recognizer()
-        with sr.Microphone() as source:
-            print("Listening...")
-            audio = r.listen(source)
 
         #Recognizing speech using recognize_google
         try:
-            command = r.recognize_google(audio)
-            print(command)
-        except sr.UnknownValueError:
-            print("Jarvis couldn't recognize you")
-        except sr.RequestError as e:
-            print(f"Jarvis error {e}")
+            with sr.Microphone() as source:
+                print("Listening...")
+                audio = r.listen(source, timeout=2, phrase_time_limit=1.5)
+                print("Recognizing...")
+            word = r.recognize_google(audio)
+            if (word.lower() == "jarvis"):
+                print(word.lower())
+                speak("Yes?")
+                with sr.Microphone() as source:
+                    print("Listening...")
+                    audio = r.listen(source)
+                    print("Recognizing...")
+                    command = r.recognize_google(audio)
+                    print(command)
+
+                    openCommand(command)
+        except Exception as e:
+            print(f"Jarvis couldn't recognize you {e}")
